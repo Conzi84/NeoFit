23acc57187a7a92e27136b597aaca400b0c38bf6
Conzi84 <bandwurmcosti@gmail.com>
2025-07-11 14:43:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    target: 'esnext'
-  },
-  server: {
-    port: 5173,
-    host: true
-  }
 })
